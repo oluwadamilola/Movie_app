43b86536a7ee55db47e5df27bb2503240027fee1
oluwadamilola <dammyolukoju@gmail.com>
2019-09-30 18:50:01 +0100

    development config here

--- webpack-util/webpack.dev.js
+++ webpack-util/webpack.dev.js
@@ -1,7 +1,15 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js/index.js');
+const common = require('./webpack.common');
+const { DefinePlugin } = require('webpack');
 
-module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'source-map'
-});
+module.exports = {
+    mode: 'development',
+    devtool: ' eval-source-map',
+    plugins: [
+        new DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development'),
+            }
+        }),
+    ],
+}
