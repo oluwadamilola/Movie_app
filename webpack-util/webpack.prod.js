24b6ffab1298e3d3f09f2f1bfaec544483a6ed60
oluwadamilola <dammyolukoju@gmail.com>
2019-09-30 18:51:15 +0100

    prodection config file

--- webpack-util/webpack.prod.js
+++ webpack-util/webpack.prod.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
+const { DefinePlugin } = require('webpack');
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production'),
+            }
+        }),
+    ],
+
 });
