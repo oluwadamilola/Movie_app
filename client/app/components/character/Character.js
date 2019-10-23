886342b194042016ded4a3728ef6b51e23cf4a08
oluwadamilola <dammyolukoju@gmail.com>
2019-10-23 07:12:10 +0100

    added more features to characters

--- client/app/components/character/Character.js
+++ client/app/components/character/Character.js
@@ -1,26 +1,34 @@
 import React from "react";
+import "./characters.scss";
 
-const Character =()=>{
-  
+function Character(props) {
     const {
       id,
       city,
       species,
       status,
       name,
-      image
-    } = this.props.location.state;
-    
+      image,
+      gender,
+      location,
+      created
+    } = props.location.state;
 
     return (
       <>
-        <p>This is a page for a single character</p>
-        <p>The selected character is {this.props.match.params.characterID}</p>
+       <div className="character-info">
+        <p>The selected character is {props.match.params.characterID}</p>
         <img src={image} alt={city} />
-        <h2>Name:{name}</h2>
-        <p>States:{status}</p>
-        <p>Species:{species}</p>
+        <h2>Name: {name}</h2>
+        <p>States: {status}</p>
+        <p>Species: {species}</p>
+        <p>Gender: {gender}</p>
+        <p>Location: {location.name}</p>
+        <span>Location: {location.url}</span>
+        <p>Created: {created}</p>
+        </div>
       </>
     );
   }
 
+export default Character;
