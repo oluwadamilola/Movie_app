c681756fdf523c78e01f3a855e2c23da88763909
oluwadamilola <dammyolukoju@gmail.com>
2019-10-30 12:25:45 +0100

    remove repition in route link

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Characters from './mainCharacters/Characters'
+import Characters from './characters/Characters'
 import Character from './character/Character'
 import '../styles/app.scss';
-import "@babel/polyfill";
+
 import {Route} from 'react-router-dom';
 
 const App = () => (
   <>
-   <Route path ="/" exact={true} component={Characters}/>
-   <Route path="/character/:characterID" exact={true} component={Character} />
+   <Route path ="/" exact component={Characters}/>
+   <Route path="/character/:characterID" exact component={Character} />
   </>
 )
 
