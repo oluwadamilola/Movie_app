fae18f2eaf3d0266c774fe787aaf84ba17534f49
oluwadamilola <dammyolukoju@gmail.com>
2019-10-30 12:28:23 +0100

    modified character feature

--- client/app/components/character/Character.js
+++ client/app/components/character/Character.js
@@ -2,34 +2,33 @@ import React from "react";
 import "./character.scss";
 
 function Character(props) {
-    const {
-      id,
-      city,
-      species,
-      status,
-      name,
-      image,
-      gender,
-      location,
-      created
-    } = props.location.state;
+  const {
+    city,
+    species,
+    status,
+    name,
+    image,
+    gender,
+    location,
+    created
+  } = props.location.state;
 
-    return (
-      <>
-       <div className="character-info">
-        <img src={image} alt={city} />
-        <h2>Name: {name}</h2>
-        <ul>
+  return ( 
+    <>
+    <div className="character-info">
+      <img src={image} alt={city} />
+      <h2>Name: {name}</h2>
+      <ul>
         <li>Status: {status}</li>
         <li>Species: {species}</li>
         <li>Gender: {gender}</li>
         <li>Location: {location.name}</li>
         <li>Created: {created}</li>
-        <span>Location Url: {location.url}</span>
-        </ul>
-        </div>
-      </>
-    );
-  }
+      </ul>
+      <span>Location Url: {location.url}</span>
+    </div>
+    </>
+  );
+}
 
 export default Character;
