dda5703bc4f3dc71498c03e47c3066d6e980cfd3
oluwadamilola <dammyolukoju@gmail.com>
2019-11-05 12:55:31 +0100

    feature(implement-loading-state): Refactor

--- client/app/components/characters/Characters.js
+++ client/app/components/characters/Characters.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { AtomSpinner } from "react-epic-spinners";
 
 import "./characters.scss";
-import api from "../../api/api";
 import CardList from "../shared/CardList";
 import useApiData from "../shared/useApiData"
 
