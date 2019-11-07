efbba8dc7b312a03fa27f7289f7ca5eb285127d3
oluwadamilola <dammyolukoju@gmail.com>
2019-11-07 13:33:41 +0100

    feature(implement-character page):refactor

--- client/app/components/character/Character.js
+++ client/app/components/character/Character.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import "./character.scss";
 
- Character =(props)=> {
+ const Character =(props)=> {
   const {
     city,
     species,
