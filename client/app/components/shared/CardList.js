71b2b6658406d57d151602eb84e34f18f489e860
oluwadamilola <dammyolukoju@gmail.com>
2019-11-08 01:15:06 +0100

    feature(implement-card-list):refactor

--- client/app/components/shared/CardList.js
+++ client/app/components/shared/CardList.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
- const CardList = ({ items })=> {
+const CardList = ({ items }) => {
 
-    const links = items.map(item => (
-        <div key={item.id}>
-            <Link
-                to={{
-                    pathname: `/character/${item.id}`,
-                    state: {
-                        ...item
-                    }
-                }}
-            >
-                <img src={item.image} alt={item.name} className="characters-img" />
-            </Link>
+  const links = items.map(item => (
+    <div key={item.id}>
+      <Link
+        to={{
+          pathname: `/character/${item.id}`,
+          state: {
+            ...item
+          }
+        }}
+      >
+        <img src={item.image} alt={item.name} className="characters-img" />
+      </Link>
 
-            <h3>{item.name}</h3>
-        </div>
-    ));
+      <h3>{item.name}</h3>
+    </div>
+  ));
 
-    return links;
+  return links;
 }
 
 export default CardList;
