540a742132cef979f075983cb73bd2f775b7e8b1
oluwadamilola <dammyolukoju@gmail.com>
2019-11-17 15:55:40 +0100

    feature(implement-character-page):refactor

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Characters from './characters/Characters'
-import Character from './character/Character'
-import '../styles/app.scss';
-
-import {Route} from 'react-router-dom';
+import React from "react";
+import Characters from "./characters/Characters";
+import Character from "./character/Character";
+import Episode from "./episodes/Episode";
+import { Route } from "react-router-dom";
+import "../styles/app.scss";
 
 const App = () => (
   <>
-   <Route path ="/" exact component={Characters}/>
-   <Route path="/character/:characterID" exact component={Character} />
+    <Route path="/" exact component={Characters} />
+    <Route path="/character/:characterID" exact component={Character} />
+    <Route path="/episodes" exact component={Episode} />
   </>
-)
-
+);
 
 export default App;
