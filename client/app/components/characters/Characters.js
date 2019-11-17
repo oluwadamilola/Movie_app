1fb528ea874f4babad7cbb6570426ed1db87be4d
oluwadamilola <dammyolukoju@gmail.com>
2019-11-17 15:56:49 +0100

    feature(implement-character-search): refactor

--- client/app/components/characters/Characters.js
+++ client/app/components/characters/Characters.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./characters.scss";
 import CardList from "../shared/CardList";
-import useApiData from "../../state/useApiData";
-import Search from "../shared/Search"
+import useApiData, { useSearchableAPI } from "../../state/useApiData";
+import Search from "../shared/Search";
+import styled from "styled-components";
 
 const Characters = () => {
-const {data,loading} = useApiData()
+  const [search, setSearch] = useState("");
+  const { data, loading } = useSearchableAPI({ name: search });
+
+  const onSearchChanged = searchTerm => {
+    setSearch(searchTerm);
+  };
+
   return (
     <>
-   < Search />
-   {loading && <p>Loading...</p>}
-    <div className="characters">
-       <CardList items={data} />
-          </div>
-
+      <Link to="/episodes">Episodes</Link>
+      <Search onSearchSubmitted={onSearchChanged} />
+      {loading && <p>Loading...</p>}
+      <div className="characters">
+        <CardList items={data} />
+      </div>
     </>
   );
 };
