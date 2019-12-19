4096abee6007eb97b8fc870affe0b7f2d6fc5b13
oluwadamilola <dammyolukoju@gmail.com>
2019-12-19 02:41:14 +0100

    feature(implement-header):refactor

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -3,10 +3,12 @@ import Characters from "./characters/Characters";
 import Character from "./character/Character";
 import Episode from "./episodes/Episode";
 import { Route } from "react-router-dom";
+import Header from "./shared/Header"
 import "../styles/app.scss";
 
 const App = () => (
   <>
+  <Header/>
     <Route path="/" exact component={Characters} />
     <Route path="/character/:characterID" exact component={Character} />
     <Route path="/episodes" exact component={Episode} />
