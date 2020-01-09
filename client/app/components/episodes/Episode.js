bfdb4438e1204db3fa664b195f826a2830753966
oluwadamilola <dammyolukoju@gmail.com>
2020-01-09 01:09:26 +0100

    feature(implement-episodes-using useReducer hook):refactor

--- client/app/components/episodes/Episode.js
+++ client/app/components/episodes/Episode.js
@@ -1,53 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import api from "../../api/api";
+import React, { useState, useEffect, useReducer } from "react";
 import EpisodesList from "./EpisodesList";
-import Pagination from './Pagination';
+import usePagedEpisodes from "../state/usePagedEpisodes";
 
-const Episode = () => {
-  const [episodes, setEpisodes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [currentPage, setcurrentPage] = useState(1);
-  const [episodesPerPage, setepisodesPerPage] = useState(5);
-  const [error, setError] = useState();
 
-  useEffect(() => {
-    const fetchEpisodes = async () => {
-      setLoading(true);
-
-      try {
-        const response = await api.get("/episode");
-        setEpisodes(response.data.results);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchEpisodes();
-  }, []);
-
-  const lastEpisode = currentPage * episodesPerPage;
-  const firstEpisode = lastEpisode - episodesPerPage;
-  const currentEpisode = episodes.slice(
-    firstEpisode,
-    lastEpisode
-  );
-
-  const paginate = (pageNumbers) =>
-    setcurrentPage(pageNumbers)
-  
-
-  if (loading) {
-    return 'Loading';
-  }
-
-  return (
+const Episode =()=>{
+const {previous,next} = usePagedEpisodes()
+return(
     <>
-      <EpisodesList items={currentEpisode} />
-      <Pagination episodesPerPage={episodesPerPage} total={episodes.length} paginate={paginate}/>
-    </>
-  );
-};
+  <EpisodesList/>
+  
+  </>
+  )
+}
 
 export default Episode;
