d2dab7785a33f2dd743caec9c5700b0972c541d5
oluwadamilola <dammyolukoju@gmail.com>
2020-01-09 01:13:17 +0100

    Feature(implement-pagation-in-episodeList):Refactor

--- client/app/components/episodes/EpisodesList.js
+++ client/app/components/episodes/EpisodesList.js
@@ -1,36 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./table.css";
+import usePagedEpisodes from "../state/usePagedEpisodes";
 
-const EpisodesList = ({ items }) => {
+
+function EpisodesList() {
+  const { episodes, previous, next } = usePagedEpisodes();
+
   return (
-    <table className="episodeTable">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Air Date</th>
-          <th>Created</th>
-          <th>Episode</th>
-          <th>Url</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(item => {
-          const { id, name, air_date, created, episode, url } = item;
-          return (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{air_date}</td>
-              <td>{created}</td>
-              <td>{episode}</td>
-              <td>{url}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className="episodeTable">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Air Date</th>
+            <th>Created</th>
+            <th>Episode</th>
+            <th>Url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {episodes.map(item => {
+            const { id, name, air_date, created, episode, url } = item;
+            return (
+              <tr key={id}>
+                <td>{name}</td>
+                <td>{air_date}</td>
+                <td>{created}</td>
+                <td>{episode}</td>
+                <td>{url}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={previous}>Prev</button>
+      <button onClick={next}>Next</button>
+    </>
   );
-};
+}
 
 EpisodesList.propTypes = {
   name: PropTypes.string,
@@ -39,4 +47,5 @@ EpisodesList.propTypes = {
   episode: PropTypes.string,
   url: PropTypes.string
 };
+
 export default EpisodesList;
