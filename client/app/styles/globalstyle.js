4097e0fafd5501a130f8dc4d0cad2a1a775b9d8a
oluwadamilola <dammyolukoju@gmail.com>
2020-01-24 00:35:40 +0100

    chore(initial-commit)installed package

---
+++ client/app/styles/globalstyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle `
+@import url('https://fonts.googleapis.com/css?family=Francois+One|Kanit');
+html {
+      box-sizing: border-box;
+      font-size: 62.5%;
+}
+*, :after, :before {
+    box-sizing: border-box;
+}
+body {
+    margin: 0;
+    padding: 0;
+    background-color: #fff;
+    color: #3c414d;
+    font-family: Francois One, Helvetica, Arial, sans-serif !important;
+    font-size: 1.2rem;
+  }
+`
+export default GlobalStyle
---
+++ client/app/styles/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const StyledButton = styled.button `
+    background-color: rgb(147, 209, 245);
+    border: 1px solid rgb(147, 209, 245);
+    margin: 1px;
+    padding: 0.20em 0.25em;
+    color: white;
+    margin-left: 1rem;
+  `;
