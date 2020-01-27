2f6265b10e451d3a471f6d336be4bc242faa17a8
oluwadamilola <dammyolukoju@gmail.com>
2020-01-27 12:51:40 +0100

    feature(implement-characters-page):refactor

--- client/app/components/characters/Characters.js
+++ client/app/components/characters/Characters.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import CardList from "../shared/CardList";
 import useSearchableAPI from "../../hooks/useSearchableAPI"
 import Search from "../shared/Search";
--- client/app/components/shared/CardList.js
+++ client/app/components/shared/CardList.js
@@ -1,26 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import {CardStyle} from '../../styles/page'
 
 const CardList = ({ items }) => {
-
-  const CardStyle = styled.div`
-    border: "1px solid #0003";
-    width: "fit-content";
-    margin-bottom: "5px";
-    border-radius: "4px";
-    margin: " 20px";
-    box-shadow: "0 5px 5px 5px #0003";
-    overflow: "hidden";
-    margin-right: " margin-right: 3%";
   
-`
   const hstyle = {
     margin: "5px"
   };
 
   return items.map(item => (
-    <CardStyle key={item.id}>
+    <CardStyle  key={item.id}>
       <Link
         to={{
           pathname: `/character/${item.id}`,
