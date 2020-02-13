710340242d1dca01d45d5cf9ccdd90b52bfbfb6d
oluwadamilola <dammyolukoju@gmail.com>
2020-02-14 00:18:38 +0100

    feature(implement-card): refactor

--- client/app/components/shared/CardList.js
+++ client/app/components/shared/CardList.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { CardStyle } from "../../styles/page";
+import styled from "styled-components"
 
-
+const CardStyle = styled.div `
+    border: 1px solid #0003;
+    width: fit-content;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    margin: 20px;
+    box-shadow: 0 5px 5px 5px #0003;
+    overflow: hidden;
+    margin-right: 3%
+  
+`
 const CardList = ({ items }) => {
   return items.map(item => (
     <CardStyle key={item.id}>
